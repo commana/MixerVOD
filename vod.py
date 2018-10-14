878b65e039297b8b231db1712aa8f06a12149cb0
Christoph Thelen <brentarus@techmastery.de>
2018-10-14 18:09:39 +0200

    Export date sorted JSON array of available recordings

--- vod.py
+++ vod.py
@@ -1,15 +1,18 @@
 import requests
-import sys
-import datetime
+import json
+import os
 
 """
 Extract all available recordings by date
 """
 
-s = requests.Session()
-s.headers.update({'Client-ID': '21c05a70d3ad447497efda7723a429cfc0da4cbd721cfaae'})
+client_id = os.environ['MIXER_API_CLIENT_ID']
+channel_id = os.environ['MIXER_API_CHANNEL_ID']
 
-channel_response = s.get('https://mixer.com/api/v1/recordings?where=channelId:eq:47094669')
+s = requests.Session()
+s.headers.update({'Client-ID': client_id})
+
+recordings_response = s.get('https://mixer.com/api/v1/recordings?where=channelId:eq:{}'.format(channel_id))
 
 # All recordings available on the channel
 # Key properties:
@@ -17,19 +20,23 @@ channel_response = s.get('https://mixer.com/api/v1/recordings?where=channelId:eq
 #   - state == AVAILABLE
 #   - name
 #   - vods[] - baseUrl - source.mp4
-recordings = channel_response.json()
+recordings = recordings_response.json()
 
 recording_infos = []
 for rec in recordings:
     if not rec["state"] == "AVAILABLE":
         continue
+    # We assume that all VODs have the same baseUrl
     video_url = rec["vods"][0]["baseUrl"] + "source.mp4"
 
     id = rec["id"]
     name = rec["name"]
     createdAt = rec["createdAt"]
-    # The seconds part and the timezone identifier are hard coded, this could fail at some point
-    createdAtDate = datetime.datetime.strptime(createdAt, "%Y-%m-%dT%H:%M:%S.000Z")
-    recording_infos.append({"id":id, "createdAt":createdAtDate})
+    recording_infos.append({"id":id, "name":name, "createdAt":createdAt, "url":video_url})
 
-print sorted(recording_infos, key=lambda rec: rec["createdAt"])
+sorted_recordings = sorted(recording_infos, key=lambda rec: rec["createdAt"])
+
+# We now have a list of recordings, sorted by date.
+# We export the list as JSON to be picked up by the next script.
+
+print json.dumps(sorted_recordings)
