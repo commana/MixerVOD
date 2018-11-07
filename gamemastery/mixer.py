0d4f6875a8f035b5e4b4ee6a77bfd93ce2d8a457
Christoph Thelen <brentarus@techmastery.de>
2018-11-07 23:52:25 +0100

    Rename and add new env variables to init SQS queue

--- gamemastery/mixer.py
+++ gamemastery/mixer.py
@@ -12,8 +12,10 @@ def handler(event, context):
     """Download recording info from Mixer.com API and initiate analysis"""
 
     IS_OFFLINE = os.environ.get('IS_OFFLINE')
-    mixer_client_id = os.environ['MIXER_API_CLIENT_ID']
-    mixer_channel_id = os.environ['MIXER_API_CHANNEL_ID']
+    gma_mixer_client_id = os.environ['GMA_MIXER_API_CLIENT_ID']
+    gma_mixer_channel_id = os.environ['GMA_MIXER_API_CHANNEL_ID']
+    gma_aws_db_table_name = os.environ['GMA_AWS_DB_TABLE_NAME']
+    gma_aws_download_queue_name = os.environ['GMA_AWS_DOWNLOAD_QUEUE_NAME']
     #s3_bucket_name = os.environ['MIXER_S3_BUCKET']
 
     if IS_OFFLINE:
@@ -21,7 +23,8 @@ def handler(event, context):
     else:
         db = boto3.resource('dynamodb')
     s3 = boto3.resource('s3')
+    sqs = boto3.resource('sqs')
 
-    recordings = gmr.get_recordings(mixer_client_id, mixer_channel_id)
-    b = gb.Bouncer(gaa.AWSStore(db, "GameRecordings"), None)
+    recordings = gmr.get_recordings(gma_mixer_client_id, gma_mixer_channel_id)
+    b = gb.Bouncer(gaa.AWSStore(db, gma_aws_db_table_name), sqs.get_queue_by_name(QueueName=gma_aws_download_queue_name))
     b.check(recordings)
